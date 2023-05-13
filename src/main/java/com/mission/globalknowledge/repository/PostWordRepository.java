5f5a27ea77f1dfacc7eae6cf67f3a590a481cdd3
Livelyoneweek <wlvvwhs@naver.com>
2023-05-14 03:00:08 +0900

    # find 메소드 추가

--- src/main/java/com/mission/globalknowledge/repository/PostWordRepository.java
+++ src/main/java/com/mission/globalknowledge/repository/PostWordRepository.java
@@ -3,5 +3,10 @@ package com.mission.globalknowledge.repository;
 import com.mission.globalknowledge.entity.PostWord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostWordRepository extends JpaRepository<PostWord,Long> {
+
+    List<Long> findAllPostId();
+    List<String> findAllWordByPostId(Long postId);
 }
