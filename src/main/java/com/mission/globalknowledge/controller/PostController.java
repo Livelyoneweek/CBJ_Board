d269686b482f13003b7e24b2bce4cd270e0639fe
Livelyoneweek <wlvvwhs@naver.com>
2023-05-14 00:59:56 +0900

    # postWord save 실행 추가

--- src/main/java/com/mission/globalknowledge/controller/PostController.java
+++ src/main/java/com/mission/globalknowledge/controller/PostController.java
@@ -3,6 +3,7 @@ package com.mission.globalknowledge.controller;
 import com.mission.globalknowledge.dto.PostDto;
 import com.mission.globalknowledge.entity.Post;
 import com.mission.globalknowledge.service.PostService;
+import com.mission.globalknowledge.service.PostWordService;
 import com.mission.globalknowledge.util.TxId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +22,12 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final PostWordService postWordService;
     private final TxId txId;
 
+    /**
+     * 게시글 리스트 조회
+     */
     @GetMapping("/post")
     public ResponseEntity<?> findAll() {
         log.info("###_{} PostController findAll",txId);
@@ -30,6 +35,9 @@ public class PostController {
         return new ResponseEntity<>(postList, HttpStatus.OK);
     }
 
+    /**
+     * 게사글 조회
+     */
     @GetMapping("/post/{id}")
     public ResponseEntity<?> findPost(@PathVariable(value = "id")Long id) {
         log.info("###_{} PostController findPost",txId);
@@ -37,10 +45,14 @@ public class PostController {
         return new ResponseEntity<>(post, HttpStatus.OK);
     }
 
+    // Post 저장 & PostWord
     @PostMapping("/post")
     public ResponseEntity<?> save(PostDto.Request.Save postDto) {
         log.info("###_{} PostController save",txId);
-        postService.save(postDto);
+
+        Post post = postService.save(postDto); // post 저장
+        postWordService.save(postDto, post); // postWord 저장
+        
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
