80630582ea968472af5a4b32e742c0dae8e613cc
Livelyoneweek <wlvvwhs@naver.com>
2023-05-14 17:29:33 +0900

    # query 작성

--- src/main/java/com/mission/globalknowledge/repository/PostWordRepository.java
+++ src/main/java/com/mission/globalknowledge/repository/PostWordRepository.java
@@ -2,11 +2,15 @@ package com.mission.globalknowledge.repository;
 
 import com.mission.globalknowledge.entity.PostWord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostWordRepository extends JpaRepository<PostWord,Long> {
 
+    @Query("select p.postId from PostWord p")
     List<Long> findAllPostId();
+
+    @Query("select p.word from PostWord p where p.postId = :postId")
     List<String> findAllWordByPostId(Long postId);
 }
