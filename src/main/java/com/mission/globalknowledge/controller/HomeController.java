16fff258ee1f39d5cce74282a9e5be517d14e84b
Livelyoneweek <wlvvwhs@naver.com>
2023-05-14 17:30:56 +0900

    # page 단 추가

--- src/main/java/com/mission/globalknowledge/controller/HomeController.java
+++ src/main/java/com/mission/globalknowledge/controller/HomeController.java
@@ -1,4 +1,19 @@
 package com.mission.globalknowledge.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/page/{id}")
+    public String gotPost(@PathVariable(value = "id")Long id) {
+        return "page";
+    }
 }
